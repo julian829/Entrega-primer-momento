ee07ac53ded9e32dcfa8bd7c45a75af42382e30f
julian <juliangiraldobravo@gmail.com>
2026-03-08 12:49:34 -0500

    creacion del menu principal

--- astrologic/astrologic.py
+++ astrologic/astrologic.py
@@ -1,2 +1,10 @@
 print("Astrologic")
 print("Entrega Python 1")
+
+def menu():
+    print("=== ASTROLOGIC ===")
+    print("1. Registrar evento")
+    print("2. Ver eventos")
+    print("3. Salir")
+
+menu()
